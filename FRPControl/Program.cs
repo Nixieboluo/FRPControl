9b0800c3946e39d82032918926b82334d9d94382
Nixieboluo <me@sagirii.me>
2024-11-05 19:09:31 +0800

    chore: add basic formatting and linting tools

--- FRPControl/Program.cs
+++ FRPControl/Program.cs
@@ -1,47 +1,31 @@
 using Scalar.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger(options =>
+    private static async Task Main(string[] args)
     {
-        options.RouteTemplate = "/openapi/{documentName}.json";
-    });
-    app.MapScalarApiReference();
-}
+        var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+        builder.Services.AddControllers();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        var app = builder.Build();
 
-app.Run();
+        // Expose OpenAPI docs
+        app.UseSwagger(options =>
+        {
+            options.RouteTemplate = "/openapi/{documentName}.json";
+        });
+        app.MapScalarApiReference();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        app.UseHttpsRedirection();
+        app.UseWebSockets();
+
+        app.UseRouting();
+        app.MapControllers();
+
+        await app.RunAsync();
+    }
 }
---
+++ FRPControl/RealtimeControl/RealtimeControlController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace FRPControl.RealtimeControl;
+
+public class RealtimeControlController : ControllerBase
+{
+    /// <summary>
+    ///     Realtime control endpoint
+    /// </summary>
+    [Route("/v1/control")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            WebSocket ws = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            await HandleMessage(ws);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+
+    private static async Task HandleMessage(WebSocket ws)
+    {
+        byte[] buffer = new byte[1024 * 4];
+        WebSocketReceiveResult result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!result.CloseStatus.HasValue)
+        {
+            await ws.SendAsync(
+                new ArraySegment<byte>(buffer, 0, result.Count),
+                result.MessageType,
+                result.EndOfMessage,
+                CancellationToken.None
+            );
+
+            result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+    }
+}
